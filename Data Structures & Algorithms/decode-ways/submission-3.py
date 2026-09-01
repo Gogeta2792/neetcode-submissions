cd61088975b3ef3d33b90d7077fbbcdb8e0fdcdc
Vincent Siregar <78709917+Gogeta2792@users.noreply.github.com>
2026-09-01 11:09:59 +0800

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        one, two = 1, 0
+
+        for i in range(len(s) - 1, -1, -1):
+            tmp = one
+            one =   (one if int(s[i]) != 0 else 0) + \
+                    (two if 10 <= int(s[i:i+2]) <= 26 else 0)
+            two = tmp
+        
+        return one
