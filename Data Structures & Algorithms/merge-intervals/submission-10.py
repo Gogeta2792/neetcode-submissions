a6b655036b7b0c31a6972c7c8144ed7fc7aa1f64
Vincent Siregar <78709917+Gogeta2792@users.noreply.github.com>
2026-09-09 20:27:51 +0800

    Add: merge-intervals - submission-10

---
+++ Data Structures & Algorithms/merge-intervals/submission-10.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_intervals = sorted(intervals, key = lambda interval: interval[0])
+        prevStart, prevEnd = sorted_intervals[0][0], sorted_intervals[0][1]
+
+        merged = []
+
+        for i in range(1, len(sorted_intervals)):
+            currStart, currEnd = sorted_intervals[i][0], sorted_intervals[i][1]
+
+            if  prevEnd >= currStart: # There is overlap, hence update the prev accordingly
+                prevStart = min(prevStart, currStart)
+                prevEnd = max(prevEnd, currEnd)
+                continue
+
+            if prevEnd < currStart: # There is no overlap, so append the prev to merged
+                merged.append([prevStart, prevEnd])
+                
+            prevStart, prevEnd = currStart, currEnd
+        
+        merged.append([prevStart, prevEnd])
+                
+        
+        return merged
