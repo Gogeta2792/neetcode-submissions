2360631cdbd8662544a71d919c2476c5bd084332
Vincent Siregar <78709917+Gogeta2792@users.noreply.github.com>
2026-09-10 14:05:48 +0800

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def helper(curr, start, combination):
+            if curr == 0:
+                res.append(combination)
+            if curr < 0:
+                return
+            if curr > 0:
+                for i, num in enumerate(nums[start:]):
+                    helper(curr - num, i + start, combination + [num])
+        
+        helper(target, 0, [])
+        
+        return res
