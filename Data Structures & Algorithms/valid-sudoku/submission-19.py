9f798ad60d5f7f38ff0410cf2b0d9da6e53b4b6f
Vincent Siregar <78709917+Gogeta2792@users.noreply.github.com>
2026-09-12 19:27:01 +0800

    Add: valid-sudoku - submission-19

---
+++ Data Structures & Algorithms/valid-sudoku/submission-19.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_hash = defaultdict(set)
+        col_hash = defaultdict(set)
+        box_hash = defaultdict(set)
+
+        rows, cols = len(board), len(board[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                val = board[r][c]
+
+                if val == ".":
+                    continue
+
+                if val in row_hash[r] or \
+                val in col_hash[c] or \
+                val in box_hash[(r // 3, c // 3)]:
+                    return False
+
+                row_hash[r].add(val)
+                col_hash[c].add(val)
+                box_hash[(r//3,c//3)].add(val)
+        
+        return True
