f94346afd23ee8ff8f1f1a55ef9fe5ec19a94fe2
Vincent Siregar <78709917+Gogeta2792@users.noreply.github.com>
2026-09-15 16:41:11 +0800

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        distance = 0
+        for i in range(len(nums) - 2, -1, -1):
+            distance += 1
+            if nums[i] >= distance:
+                distance = 0
+            
+        return nums[0] >= distance
