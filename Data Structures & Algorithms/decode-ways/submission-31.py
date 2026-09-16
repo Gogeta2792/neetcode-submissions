0e1e3bef49ebd080de60c2abf96b42e4d2f2bc26
Vincent Siregar <78709917+Gogeta2792@users.noreply.github.com>
2026-09-16 17:54:37 +0800

    Add: decode-ways - submission-31

---
+++ Data Structures & Algorithms/decode-ways/submission-31.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+        def helper(i):
+            if i in memo:
+                return memo[i]
+            if i == len(s):
+                return 1
+            else:
+                memo[i] = (helper(i+1) if s[i] != "0" else 0) + (helper(i+2) if 10 <= int(s[i:i+2]) <= 26 else 0)
+                return memo[i]
+        return helper(0)
